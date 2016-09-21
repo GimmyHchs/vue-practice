ca41d09889c2e76eb5b3d24aa9978afef413bbd9
g9308370 <g9308370@hotmail.com>
2016-09-21 10:15:03 +0800

    bootstrap error

---
+++ resources/views/vue/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>vue default</title>
+    </head>
+    <body>
+        <default></default>
+    </body>
+    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{asset('css/app.css')}}"></script>
+</html>
