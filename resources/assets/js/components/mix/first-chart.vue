f4a26e0823e9cd673f83a9841c02375daba1ef12
g9308370 <g9308370@hotmail.com>
2016-10-06 14:36:21 +0800

    basic Harcode Chart Done

--- resources/assets/js/components/mix/first-chart.vue
+++ resources/assets/js/components/mix/first-chart.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">First Chart Component</div>
-
-                    <div class="panel-body">
-                        I'm an First Chart Component
-                    </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">First Chart Component</div>
+                <div class="panel-body">
+                    I'm an First Chart Component
+                    <div>{{chart_data.datasets[0].data}}</div>
+                    <button type="button" @click="renderChart()">Rendor Line Chart</button>
                 </div>
+                <canvas id="canvas" width="300" height="300">
+                </canvas>
             </div>
         </div>
     </div>
@@ -16,6 +17,9 @@
 
 <script>
     export default {
+        mixins:[
+            first_chart.default,
+        ],
         mounted() {
             console.log('First Chart Component ready.')
         }
--- resources/assets/js/mixin/first-chart.js
+++ resources/assets/js/mixin/first-chart.js
@@ -1,12 +1,49 @@
-export default {
-    data : {
-        title:'hello',
-        body:'nice to meet you!!!',
-        chart: null,
+export default{
+    data () {
+        return {
+            title:'hello',
+            body:'nice to meet you!!!',
+            chart: null,
+            canvas:null,
+            chart_data : {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [
+                    {
+                        label: "My First dataset",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        borderColor: "rgba(75,192,192,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: [65, 59, 80, 81, 56, 55, 40],
+                        spanGaps: false,
+                    }
+                ]
+            },
+        };
     },
     methods:{
-        renderChart:function(){
-            this.chart = new Chart();
+        renderChart() {
+            this.chart = new Chart(this.canvas, {
+                type: 'line',
+                data: this.chart_data,
+            });
         },
+    },
+    mounted(){
+        this.canvas = document.querySelector('#canvas').getContext('2d');
+        // this.renderChart(this.canvas);
     }
 }
