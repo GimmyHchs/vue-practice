c8d0e57495064d97056db87121c1fcb5b81f30b7
g9308370 <g9308370@hotmail.com>
2016-10-11 09:07:01 +0800

    Messages

--- resources/views/vue/mix/charts.blade.php
+++ resources/views/vue/mix/charts.blade.php
@@ -39,11 +39,17 @@
                 </ul>
             </div>
             <div class="col-md-4">
-
+                <h2>Message</h2>
+                <input type="checkbox" id="shutup" v-model="messageToggle">
+                <label for="shutup">Shut Up</label>
+                <button @click="cleanMessage()" type="button" class="btn btn-default">Clean Message</button>
+                <ul>
+                    <li v-for="value in message">@{{value}}</li>
+                </ul>
             </div>
         </div>
-    {{-- <line-chart v-show="isNowComponent('line-chart')" :labels="labels" :data="data"></line-chart>
-    <bar-chart v-show="isNowComponent('bar-chart')" :labels="labels" :data="data"></bar-chart> --}}
+    {{-- <line-chart v-show="isNowComponent('line-chart')" :labels="labels" :data="data"></line-chart> --}}
+    {{-- <bar-chart v-show="isNowComponent('bar-chart')" :labels="labels" :data="data"></bar-chart> --}}
     <component :is="now_component" :labels="labels" :data="data"></component>
     </div>
 @endsection
@@ -54,8 +60,40 @@
                 now_component:'line-chart',
                 labels: ["January", "February", "March", "April", "May", "June", "July"],
                 data:[65, 59, 80, 81, 56, 55, 40],
+                messageToggle:false,
+                message:[],
                 newData:'',
                 newLabel:'',
+            },
+            watch:{
+                labels(val){
+                    if(!this.isCombine)
+                        this.addMessage('Warning! Different Count Between Data and Labels');
+                    else
+                        this.addMessage('Ok, Nice label, I will render well');
+                },
+                data(val){
+                    if(!this.isCombine)
+                        this.addMessage('Warning! Different Count Between Data and Labels');
+                    else
+                        this.addMessage('Ok, Nice data, I will render well');
+                },
+                message(val){
+                    if(this.message.length>4)
+                        this.message.splice(0, 1);
+                }
+            },
+            computed:{
+                isCombine(){
+                    if(this.labels.length == this.data.length)
+                        return true;
+                    else
+                    {
+                        console.log('Data:'+this.data.length);
+                        console.log('Labels:'+this.labels.length);
+                        return false;
+                    }
+                },
             },
             methods:{
                 changeComponent(component){
@@ -77,6 +115,13 @@
                     this.labels.push(this.newLabel);
                     this.newLabel='';
                 },
+                addMessage(message){
+                    if(!this.messageToggle)
+                        this.message.push(message);
+                },
+                cleanMessage(){
+                    this.message=[];
+                }
             },
         }
     </script>
